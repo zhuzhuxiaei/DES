84dd0b756302e9b5d024cce3a6ba486714d94c38
zhuzhuxiaei <1433272948@qq.com>
2018-10-12 23:06:12 +0800

    Create ipExchange.cpp

---
+++ ipExchange.cpp
@@ -0,0 +1,32 @@
+/*
+*函数名: ipExchange
+*作用  :将明文进行初始置换
+*/
+#include "struct.h"
+extern bdata64 mingwen;
+extern int iptable[];
+int ipExchange()
+{	
+	bdata64 oldmingwen = mingwen;
+	char table[] = { -128,64,32,16,8,4,2,1 };
+	for (int i = 0; i < 64; i++)
+	{
+		int position = iptable[i] - 1;
+		int row = position / 8;
+		int offset = position % 8;
+		int answer = oldmingwen.bit[row] & table[offset];
+		if (answer)//该位置1
+		{
+			row = i / 8;
+			offset = i % 8;
+			mingwen.bit[row] = mingwen.bit[row] | table[offset];
+		}
+		else//该位置0
+		{
+			row = i / 8;
+			offset = i % 8;
+			mingwen.bit[row] = mingwen.bit[row] & (~table[offset]);
+		}
+	}
+	return 0;
+}
